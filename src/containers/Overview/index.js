733eed46711c927248646d425474770e715032bd
AlessandroMessori <allemex@libero.it>
2017-04-29 18:04:14 +0200

    loading fix

--- src/containers/Overview/index.js
+++ src/containers/Overview/index.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {PageHeader, Row, Col} from 'react-bootstrap'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
 import {overViewData} from '../../selectors'
+import Spinner from '../../components/Spinner'
 import {getStats} from '../../helpers/analytics'
 import './index.scss'
 
@@ -19,26 +20,31 @@ class Teacher extends React.Component {
     const {data} = this.props
     const stats = getStats(data.map(item => parseInt(item.goodVotesPercentage.substr(0, item.goodVotesPercentage.length - 1), 0)))
     const {min, max, avg} = stats
+    const {length} = data
 
     return (<section className="teacherSection">
       <PageHeader>Panoramica</PageHeader>
+      {length < 1 && <Spinner/>}
+      {length > 0 &&
+      <section>
+        <Row className='stats-section'>
+          <Col md={4}><h2>Media Massima: {max}%</h2></Col>
+          <Col md={4}><h2>Media Minima: {min}%</h2></Col>
+          <Col md={4}><h2>Media Scuola: {avg}%</h2></Col>
+        </Row>
 
-      <Row className='stats-section'>
-        <Col md={4}><h2>Media Massima: {max}%</h2></Col>
-        <Col md={4}><h2>Media Minima: {min}%</h2></Col>
-        <Col md={4}><h2>Media Scuola: {avg}%</h2></Col>
-      </Row>
+        <br/>
 
-      <br/>
-
-      <BootstrapTable className='dataTable' data={data} hover={true}>
-        <TableHeaderColumn dataField="nomeDocente" isKey={true} dataAlign="center" dataSort={true}>Nome
-          Docente</TableHeaderColumn>
-        <TableHeaderColumn dataField="goodVotesPercentage" dataAlign="center" dataSort={true}>Valore Voti
-          Positivi</TableHeaderColumn>
-        <TableHeaderColumn dataField="difference" dataAlign="center" dataSort={true}>Delta Docente
-          Scuola</TableHeaderColumn>
-      </BootstrapTable>
+        <BootstrapTable className='dataTable' data={data} hover={true}>
+          <TableHeaderColumn dataField="nomeDocente" isKey={true} dataAlign="center" dataSort={true}>Nome
+            Docente</TableHeaderColumn>
+          <TableHeaderColumn dataField="goodVotesPercentage" dataAlign="center" dataSort={true}>Valore Voti
+            Positivi</TableHeaderColumn>
+          <TableHeaderColumn dataField="difference" dataAlign="center" dataSort={true}>Delta Docente
+            Scuola</TableHeaderColumn>
+        </BootstrapTable>
+      </section>
+      }
 
     </section>)
   }
--- src/containers/Teacher/index.js
+++ src/containers/Teacher/index.js
@@ -30,42 +30,42 @@ class Teacher extends React.Component {
   }
 
   render() {
-    const {loading, visualType, tableData, barData, teacherStats} = this.props
+    const {visualType, tableData, barData, teacherStats} = this.props
     const {max, min, avg} = teacherStats
+    const {length} = tableData
 
     return (<section className="teacherSection">
 
 
       <PageHeader>{this.props.params.name}</PageHeader>
-      {loading && <Spinner/>}
-      {
-        !loading &&
-        <section>
-          <Row className='stats-section'>
-            <Col md={4}><h2>Voto Massimo: {max}%</h2></Col>
-            <Col md={4}><h2>Voto Minimo: {min}%</h2></Col>
-            <Col md={4}><h2>Media Voti: {avg}%</h2></Col>
-          </Row>
-          <ButtonGroup className='selector'>
-            <Button active={visualType === 'chart'}
-                    onClick={() => this.props.changeVisualType('chart')}>Grafico</Button>
-            <Button active={visualType === 'table'}
-                    onClick={() => this.props.changeVisualType('table')}>Tabella</Button>
-          </ButtonGroup>
-          <br/>
-          {visualType === 'chart' &&
-          <Bar data={barData} width='700' height='400'/>}
-          {visualType === 'table' && <BootstrapTable className='dataTable' data={tableData} hover={true}>
-            <TableHeaderColumn dataField="idDomanda" isKey={true} dataAlign="center" dataSort={true}>ID
-              Domanda</TableHeaderColumn>
-            <TableHeaderColumn dataField="goodVotesPercentage" dataAlign="center" dataSort={true}>Valore Voti
-              Positivi</TableHeaderColumn>
-            <TableHeaderColumn dataField="schoolPercentage" dataAlign="center" dataSort={true}>Media della
-              Scuola</TableHeaderColumn>
-            <TableHeaderColumn dataField="difference" dataAlign="center" dataSort={true}>Delta Docente
-              Scuola</TableHeaderColumn>
-          </BootstrapTable>}
-        </section>
+      {length < 1 && <Spinner/>}
+      {length > 0 &&
+      <section>
+        <Row className='stats-section'>
+          <Col md={4}><h2>Voto Massimo: {max}%</h2></Col>
+          <Col md={4}><h2>Voto Minimo: {min}%</h2></Col>
+          <Col md={4}><h2>Media Voti: {avg}%</h2></Col>
+        </Row>
+        <ButtonGroup className='selector'>
+          <Button active={visualType === 'chart'}
+                  onClick={() => this.props.changeVisualType('chart')}>Grafico</Button>
+          <Button active={visualType === 'table'}
+                  onClick={() => this.props.changeVisualType('table')}>Tabella</Button>
+        </ButtonGroup>
+        <br/>
+        {visualType === 'chart' &&
+        <Bar data={barData} width='700' height='400'/>}
+        {visualType === 'table' && <BootstrapTable className='dataTable' data={tableData} hover={true}>
+          <TableHeaderColumn dataField="idDomanda" isKey={true} dataAlign="center" dataSort={true}>ID
+            Domanda</TableHeaderColumn>
+          <TableHeaderColumn dataField="goodVotesPercentage" dataAlign="center" dataSort={true}>Valore Voti
+            Positivi</TableHeaderColumn>
+          <TableHeaderColumn dataField="schoolPercentage" dataAlign="center" dataSort={true}>Media della
+            Scuola</TableHeaderColumn>
+          <TableHeaderColumn dataField="difference" dataAlign="center" dataSort={true}>Delta Docente
+            Scuola</TableHeaderColumn>
+        </BootstrapTable>}
+      </section>
       }
     </section>)
   }
